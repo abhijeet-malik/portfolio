84117e5b091703327b62f45d17cd0607187f5e95
abhijeet-malik <abhijeetmalik4321@gmail.com>
2025-06-22 18:57:39 +0530

    final image

--- src/components/data/project.js
+++ src/components/data/project.js
@@ -15,8 +15,7 @@ export const ProjectData = [
     ],
     demo: "https://quick-rental.vercel.app/",
     github: "https://github.com/abhijeet-malik/quick-rental",
-    image:
-      "https://private-user-images.githubusercontent.com/181703567/457635946-11c43908-ff5c-41c5-8890-97542ae2d32c.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTA1ODUzMjgsIm5iZiI6MTc1MDU4NTAyOCwicGF0aCI6Ii8xODE3MDM1NjcvNDU3NjM1OTQ2LTExYzQzOTA4LWZmNWMtNDFjNS04ODkwLTk3NTQyYWUyZDMyYy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNjIyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDYyMlQwOTM3MDhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1hNDcyMWFkZjY0ZmIyOWM5NGE0MWNmNjlmYzU4ZmE2YjliNzdkY2UyMzc1OGUwMDM5MzUwYmU0MDY3NTllYTQ2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.QYGGQyOqjE1Lm6u_i5A0MXbsbzH5q4uieAuMpoE1euI",
+    image:"https://raw.githubusercontent.com/abhijeet-malik/To-Do-List/main/Bike-rental.png",
   },
   {
     id: 2,
@@ -35,7 +34,7 @@ export const ProjectData = [
     demo: "https://weather-app-kappa-lime-95.vercel.app/",
     github: "https://github.com/abhijeet-malik/weather-app",
     image:
-      "https://private-user-images.githubusercontent.com/181703567/457636109-ae2fc7bc-4f45-4374-93d2-d55335cc5532.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTA1ODU0NTAsIm5iZiI6MTc1MDU4NTE1MCwicGF0aCI6Ii8xODE3MDM1NjcvNDU3NjM2MTA5LWFlMmZjN2JjLTRmNDUtNDM3NC05M2QyLWQ1NTMzNWNjNTUzMi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNjIyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDYyMlQwOTM5MTBaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yYTdlNTY0ODZkYWZiZjJkZmVjZTNkNGY4NTZmYjUxMmQwODBhYTU2MWUxMjMyODAzMDM4Y2NmMGZjMDc1ZTAxJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.qQwII81rDMwHVluKQWfHew0_ODA5G8rJ0OaYA-47oBs",
+      "https://raw.githubusercontent.com/abhijeet-malik/To-Do-List/main/Weather-app.png",
   },
 
   {
@@ -43,11 +42,11 @@ export const ProjectData = [
     title: "Frontend Dashboard UI | Segwise",
     about:
       "Built a dynamic and responsive product dashboard using React.js and Tailwind CSS as part of Segwise’s frontend intern assessment. The challenge involved turning a Figma design into a pixel-perfect UI while adding complex interactive features like:Filtering and sorting data., A custom data table with search. , A row preview component similar to LinkedIn chat popups., Modal view on row click for detailed insights. - This project tested my UI/UX implementation skills and React logic under tight deadlines (8 hours), simulating real-world product scenarios.",
-      tags: ["HTML5", "CSS3", "JavaScript", "Node.Js", "Responsive Web Design"],
+    tags: ["HTML5", "CSS3", "JavaScript", "Node.Js", "Responsive Web Design"],
     demo: "https://segwise-assignment-sigma.vercel.app/",
     github: "https://segwise-assignment-sigma.vercel.app/",
     image:
-      "https://github.com/user-attachments/assets/62c59a15-4330-4b33-a38b-69bb05ec4282",
+      "https://raw.githubusercontent.com/abhijeet-malik/To-Do-List/main/segwise.png",
   },
   {
     id: 4,
@@ -57,6 +56,7 @@ export const ProjectData = [
     tags: ["HTML5", "CSS3", "JavaScript", "Node.Js", "Responsive Web Design"],
     demo: "https://to-do-list-lilac-iota-83.vercel.app/",
     github: "https://github.com/abhijeet-malik/To-Do-List",
-    image: "https://raw.githubusercontent.com/abhijeet-malik/To-Do-List/main/todolist.png",
+    image:
+      "https://raw.githubusercontent.com/abhijeet-malik/To-Do-List/main/todolist.png",
   },
 ];
