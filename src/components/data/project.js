a4a76f96590d82f6b17cf1982ffe20ab5b5c5eb4
abhijeet-malik <abhijeetmalik4321@gmail.com>
2025-06-22 10:43:39 +0530

    first commit

---
+++ src/components/data/project.js
@@ -0,0 +1,33 @@
+export const ProjectData = [
+    {
+        id:"1",
+        title:"Bike-Rental",
+        about:"Developed a responsive Bike Rental Website using React.js and Tailwind CSS, incorporating a seamless booking system, interactive UI animations with AOS, and a mobile-friendly navigation menu. Implemented dark mode functionality, optimized user engagement through well-structure",
+        tags:["React.js", "Tailwind CSS", "AOS","React Icons"],
+        demo:"https://abhijeet-malik.github.io/quick-bike-rental/",
+        github:"https://github.com/abhijeet-malik/quick-bike-rental",
+        image:"https://github.com/user-attachments/assets/ab4e2eb5-1891-481a-a9bf-a040f1b28050"
+    },
+
+    {
+        id: 2,
+        title: "To-Do list",
+        about: "Todo List App helps you keep a track of your Day to Day Activities.",
+        tags: ["HTML", "CSS", "JAVASCRIPT", "NODEJS"],
+        demo: "https://abhijeet-malik.github.io/To-Do-List/",
+        github: "https://github.com/abhijeet-malik/To-Do-List",
+        image: "https://github.com/user-attachments/assets/8f3528e2-e4de-4ab8-8717-1105439fc417"
+
+    },
+
+    {
+        id: 3,
+        title: "Web Quiz Application",
+        about: "Developed an interactive quiz app using JavaScript, HTML, and CSS with dynamic question rendering, answer selection, and score calculation.",
+        tags: ["HTML", "CSS", "JAVASCRIPT"],
+        demo: "https://abhijeet-malik.github.io/quizz-website/",
+        github: "https://github.com/abhijeet-malik/quizz-website",
+        image: "https://github.com/user-attachments/assets/cd8782d2-9a7f-4d9b-a09f-4f09c793f6b5"
+
+    }
+]
---
+++ src/components/data/work.js
@@ -0,0 +1,16 @@
+export const WorkData =[
+    {
+        company:"CodeCraft Technologies",
+        designation:"Intern",
+        dateJoining:"March 2025 to April 2025",
+        dateEnd:"Internship",
+        work:"Implement a user authentication system with secure login and registration functionality. Users should be able to sign up for an account, log in securely and access protected routes only after successful authentication."
+    },
+    {
+        company:"National Aluminium Company (NALCO)",
+        designation:"Intern",
+        dateJoining:"July 2023 to  September 2023",
+        dateEnd:"Internship",
+        work:"Visiting & Study about Carbon Area.- Visited and studied the electrical operations in the Carbon Area of a steel manufacturing plant. Gained practical insights into power distribution systems, control panels, motor drives, PLCs, and safety protocols involved in electrode handling, furnace control, and carbon processing systems."
+    }
+]
